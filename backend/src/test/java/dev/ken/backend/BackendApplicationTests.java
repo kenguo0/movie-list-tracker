c16e70a1b0c6c2041e4afba5368ef0481b002fd5
kenguo0 <92478370+kenguo0@users.noreply.github.com>
2023-07-06 13:19:05 -0400

    initial commit

---
+++ backend/src/test/java/dev/ken/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ken.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
