fd8e626b262e3cc855ea6238b170552b7edd7c02
kenguo0 <92478370+kenguo0@users.noreply.github.com>
2023-08-01 00:23:34 -0400

    deployment troubleshooting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: true,
-        port: 3000,
-        proxy: {
-            "/api": {
-                target: "https://movie-list-tracker.up.railway.app",
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-            },
-        },
-    },
+    // server: {
+    //     host: true,
+    //     port: 3000,
+    //     proxy: {
+    //         "/api": {
+    //             target: "https://movie-list-tracker.up.railway.app",
+    //             changeOrigin: true,
+    //             secure: false,
+    //             ws: true,
+    //         },
+    //     },
+    // },
 });
