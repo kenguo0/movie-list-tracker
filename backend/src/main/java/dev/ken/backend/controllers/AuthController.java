50a2f8dce0045275ba0e9a9428b289cc2871d046
kenguo0 <92478370+kenguo0@users.noreply.github.com>
2023-08-01 00:49:43 -0400

    updating crossorigin

--- backend/src/main/java/dev/ken/backend/controllers/AuthController.java
+++ backend/src/main/java/dev/ken/backend/controllers/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://watchverse-movielist.netlify.app")
+@CrossOrigin
 @RequestMapping("/api/auth")
 public class AuthController {
 
