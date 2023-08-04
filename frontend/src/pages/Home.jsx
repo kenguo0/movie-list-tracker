6e9d45db272ed5285073b988c6148da7b68fb2ac
kenguo0 <92478370+kenguo0@users.noreply.github.com>
2023-08-04 10:12:29 -0400

    adding fetchApiKey to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,31 @@ import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import "../styles/movieResults.css";
 
-export default function Home({ apiKey }) {
+export default function Home({ apiURL }) {
     const [popularMovies, setPopularMovies] = useState([]);
+    const [apiKey, setApiKey] = useState("");
+    const fetchApiKey = async () => {
+        try {
+            const response = await fetch(`${apiURL}/api/movie/key`, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                method: "get",
+                credentials: "include",
+            });
+            if (response.headers.get("isAuthenticatedHeader") === "true") {
+                const data = await response.text();
+                setApiKey(data);
+            } else {
+                console.log("Could not get API key", response.text());
+            }
+        } catch (error) {
+            console.error("Error fetching API key:", error);
+        }
+    };
 
     useEffect(() => {
+        fetchApiKey();
         if (apiKey !== "") {
             fetch("https://api.themoviedb.org/3/trending/movie/week", {
                 headers: {
